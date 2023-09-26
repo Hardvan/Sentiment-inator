7abb557e194e934a70a5d7d4d326927ebc6ebf70
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-09-26 18:59:16 +0530

    Add skeleton HTML

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+
+
+# Import the predict_sentiment function
+import NLP_model
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+
+    result = None
+    if request.method == 'POST':
+        result = {}
+
+        # Text input from the form
+        text_input = request.form['text_input']
+        result["text_input"] = text_input
+
+        # Predict the sentiment
+        predicted_sentiment = None
+        if text_input:
+            predicted_sentiment = NLP_model.predict_sentiment(text_input)
+        else:
+            predicted_sentiment = "Please enter some text."
+
+        result["predicted_sentiment"] = predicted_sentiment
+
+    return render_template("index.html", result=result)
 
 
 if __name__ == "__main__":
